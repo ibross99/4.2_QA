44019f5e63f8e6d9f8eb01bac34812963255df92
Ibrahim.Abdelmagied <Ibrahim.Abdelmagied@hse.gov.uk>
2025-09-08 14:32:47 +0100

    save

--- app.py
+++ app.py
@@ -80,11 +80,6 @@ def delete_request(id):
     
     return redirect(url_for('index'))
 
-if __name__ == "__main__":
-
-    with app.app_context():
-
-        db.create_all()
-
+if __name__ == '__main__':
+    init_db()
     app.run(host='0.0.0.0', port=5500, debug =True)
- 
